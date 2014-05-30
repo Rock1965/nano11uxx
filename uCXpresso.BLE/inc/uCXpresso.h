bb16e21a48395dfb876511ffc80fae448c18eb63
廖晨凱 <jason@embeda.com.tw>
2014-05-30 16:22:33 +0800

    v1.0.6c : PWM zero for read();

--- uCXpresso.BLE/inc/uCXpresso.h
+++ uCXpresso.BLE/inc/uCXpresso.h
@@ -20,8 +20,8 @@ extern "C" {
 #define uCXpresso_VER_MINOR		0
 #define uCXpresso_VER_REV		6
 #define uCXpresso_VER_RC		RELEASED
-#define uCXpresso_VER_BUILD		"2014/5/28"
-#define uCXpresso_VER_STR		"V1.0.6"
+#define uCXpresso_VER_BUILD		"2014/5/30"
+#define uCXpresso_VER_STR		"V1.0.6c"
 
 //
 // Device
