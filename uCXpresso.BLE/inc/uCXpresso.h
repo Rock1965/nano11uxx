fde64c29be110f0a92bbd57a8024cb3e5c1d0329
廖晨凱 <jason@embeda.com.tw>
2014-12-05 11:19:35 +0800

    v2.0.1 released.

--- uCXpresso.BLE/inc/uCXpresso.h
+++ uCXpresso.BLE/inc/uCXpresso.h
@@ -19,8 +19,8 @@ extern "C" {
 #define uCXpresso_VER_MAJOR		2
 #define uCXpresso_VER_MINOR		0
 #define uCXpresso_VER_REV		1
-#define uCXpresso_VER_RC		2
-#define uCXpresso_VER_BUILD		"2014/11/24"
+#define uCXpresso_VER_RC		RELEASED
+#define uCXpresso_VER_BUILD		"2014/12/25"
 #define uCXpresso_VER_STR		"V2.0.1"
 
 //
